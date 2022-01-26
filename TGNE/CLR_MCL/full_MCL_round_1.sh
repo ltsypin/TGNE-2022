13cd75545fefbc9948610a10d85fe47626aa6352
Eukarya <eukarya@dhcp-14-165.caltech.edu>
2022-01-25 21:24:24 -0800

    playing with n_neighbors=2

--- TGNE/CLR_MCL/full_MCL_round_1.sh
+++ TGNE/CLR_MCL/full_MCL_round_1.sh
@@ -1,39 +1,39 @@
 # First, take a look at what different z-score cutoffs will do to the network
-mcx query -imx full_clr_nn3_graph_round_1.mci -vary-threshold 7/20/130
+mcx query -imx full_clr_nn2_graph_round_1.mci -vary-threshold 7/20/130
 
 # It looks like 15.9 is as high as we can go without really losing genes from the network.
 
-mcx alter -imx full_clr_nn3_graph_round_1.mci -tf 'gq(15.9), add(-15.9)' -o full_clr_nn3_graph_round_1_15.9.mci
+mcx alter -imx full_clr_nn2_graph_round_1.mci -tf 'gq(15.9), add(-15.9)' -o full_clr_nn2_graph_round_1_15.9.mci
 
 
 # Now, we need to scan different inflation values for each
 
 # To assess the quality of the results, first perform coarse clustering
-clm close -imx full_clr_nn3_graph_round_1_15.9.mci --write-cc -o full_clr_nn3_graph_round_1_15.9.base
+clm close -imx full_clr_nn2_graph_round_1_15.9.mci --write-cc -o full_clr_nn2_graph_round_1_15.9.base
 
 # Cluster and check stats. We want to maximimize efficiency while keeping things reasonable
-mcl full_clr_nn3_graph_round_1_15.9.mci -I 1.2 -scheme 7
+mcl full_clr_nn2_graph_round_1_15.9.mci -I 1.2 -scheme 7
 
-mcl full_clr_nn3_graph_round_1_15.9.mci -I 1.4 -scheme 7
+mcl full_clr_nn2_graph_round_1_15.9.mci -I 1.4 -scheme 7
 
-mcl full_clr_nn3_graph_round_1_15.9.mci -I 1.6 -scheme 7
+mcl full_clr_nn2_graph_round_1_15.9.mci -I 1.6 -scheme 7
 
-mcl full_clr_nn3_graph_round_1_15.9.mci -I 1.8 -scheme 7
+mcl full_clr_nn2_graph_round_1_15.9.mci -I 1.8 -scheme 7
 
-mcl full_clr_nn3_graph_round_1_15.9.mci -I 2 -scheme 7
+mcl full_clr_nn2_graph_round_1_15.9.mci -I 2 -scheme 7
 
-mcl full_clr_nn3_graph_round_1_15.9.mci -I 2.2 -scheme 7
+mcl full_clr_nn2_graph_round_1_15.9.mci -I 2.2 -scheme 7
 
-mcl full_clr_nn3_graph_round_1_15.9.mci -I 2.4 -scheme 7
+mcl full_clr_nn2_graph_round_1_15.9.mci -I 2.4 -scheme 7
 
-mcl full_clr_nn3_graph_round_1_15.9.mci -I 2.6 -scheme 7
+mcl full_clr_nn2_graph_round_1_15.9.mci -I 2.6 -scheme 7
 
-mcl full_clr_nn3_graph_round_1_15.9.mci -I 2.8 -scheme 7
+mcl full_clr_nn2_graph_round_1_15.9.mci -I 2.8 -scheme 7
 
-mcl full_clr_nn3_graph_round_1_15.9.mci -I 3 -scheme 7
+mcl full_clr_nn2_graph_round_1_15.9.mci -I 3 -scheme 7
 
-clm info full_clr_nn3_graph_round_1_15.9.mci out.full_clr_nn3_graph_round_1_15.9.mci.I{12,14,16,18,20,22,24,26,28,30}
-clm dist --chain out.full_clr_nn3_graph_round_1_15.9.mci.I{12,14,16,18,20,22,24,26,28,30}
+clm info full_clr_nn2_graph_round_1_15.9.mci out.full_clr_nn2_graph_round_1_15.9.mci.I{12,14,16,18,20,22,24,26,28,30}
+clm dist --chain out.full_clr_nn2_graph_round_1_15.9.mci.I{12,14,16,18,20,22,24,26,28,30}
 
 # Based on the summary stats for the different approaches, it looks like -I 1.8 is the way to go here.
 # We get efficiency=0.53808 massfrac=0.46558 areafrac=0.00155. The MCL algorithm also gives "fabulous"
@@ -42,69 +42,69 @@ clm dist --chain out.full_clr_nn3_graph_round_1_15.9.mci.I{12,14,16,18,20,22,24,
 # below 1.8. I think this gives a healthy balance between wanting modules of reasonable size and
 # having the most performant clustering.
 
-mcxdump -icl out.full_clr_nn3_graph_round_1_15.9.mci.I18 -o full_labeled_MCL_round_1_clusters.txt -tabr full_clr_nn3_graph_round_1.dict
+mcxdump -icl out.full_clr_nn2_graph_round_1_15.9.mci.I18 -o full_labeled_MCL_nn2_round_1_clusters.txt -tabr full_clr_nn2_graph_round_1.dict
 
 
 # repeat process for vegetative and sexual cases!
-mcx query -imx veg_clr_nn3_graph_round_1.mci -vary-threshold 7/20/130
+mcx query -imx veg_clr_nn2_graph_round_1.mci -vary-threshold 7/20/130
 
 # Looks like a cutoff of 11.8 is the way to go here.
-mcx alter -imx veg_clr_nn3_graph_round_1.mci -tf 'gq(11.8), add(-11.8)' -o veg_clr_nn3_graph_round_1_11.8.mci
+mcx alter -imx veg_clr_nn2_graph_round_1.mci -tf 'gq(11.8), add(-11.8)' -o veg_clr_nn2_graph_round_1_11.8.mci
 
-mcl veg_clr_nn3_graph_round_1_11.8.mci -I 1.2 -scheme 7
+mcl veg_clr_nn2_graph_round_1_11.8.mci -I 1.2 -scheme 7
 
-mcl veg_clr_nn3_graph_round_1_11.8.mci -I 1.4 -scheme 7
+mcl veg_clr_nn2_graph_round_1_11.8.mci -I 1.4 -scheme 7
 
-mcl veg_clr_nn3_graph_round_1_11.8.mci -I 1.6 -scheme 7
+mcl veg_clr_nn2_graph_round_1_11.8.mci -I 1.6 -scheme 7
 
-mcl veg_clr_nn3_graph_round_1_11.8.mci -I 1.8 -scheme 7
+mcl veg_clr_nn2_graph_round_1_11.8.mci -I 1.8 -scheme 7
 
-mcl veg_clr_nn3_graph_round_1_11.8.mci -I 2 -scheme 7
+mcl veg_clr_nn2_graph_round_1_11.8.mci -I 2 -scheme 7
 
-mcl veg_clr_nn3_graph_round_1_11.8.mci -I 2.2 -scheme 7
+mcl veg_clr_nn2_graph_round_1_11.8.mci -I 2.2 -scheme 7
 
-mcl veg_clr_nn3_graph_round_1_11.8.mci -I 2.4 -scheme 7
+mcl veg_clr_nn2_graph_round_1_11.8.mci -I 2.4 -scheme 7
 
-mcl veg_clr_nn3_graph_round_1_11.8.mci -I 2.6 -scheme 7
+mcl veg_clr_nn2_graph_round_1_11.8.mci -I 2.6 -scheme 7
 
-mcl veg_clr_nn3_graph_round_1_11.8.mci -I 2.8 -scheme 7
+mcl veg_clr_nn2_graph_round_1_11.8.mci -I 2.8 -scheme 7
 
-mcl veg_clr_nn3_graph_round_1_11.8.mci -I 3 -scheme 7
+mcl veg_clr_nn2_graph_round_1_11.8.mci -I 3 -scheme 7
 
-clm info veg_clr_nn3_graph_round_1_11.8.mci out.veg_clr_nn3_graph_round_1_11.8.mci.I{12,14,16,18,20,22,24,26,28,30}
-clm dist --chain out.veg_clr_nn3_graph_round_1_11.8.mci.I{12,14,16,18,20,22,24,26,28,30}
+clm info veg_clr_nn2_graph_round_1_11.8.mci out.veg_clr_nn2_graph_round_1_11.8.mci.I{12,14,16,18,20,22,24,26,28,30}
+clm dist --chain out.veg_clr_nn2_graph_round_1_11.8.mci.I{12,14,16,18,20,22,24,26,28,30}
 
 # Here, I see that -I 1.8 is the way to go: efficiency=0.54654 massfrac=0.30571 areafrac=0.00196
-mcxdump -icl out.veg_clr_nn3_graph_round_1_11.8.mci.I18 -o veg_labeled_MCL_round_1_clusters.txt -tabr veg_clr_nn3_graph_round_1.dict 
+mcxdump -icl out.veg_clr_nn2_graph_round_1_11.8.mci.I18 -o veg_labeled_MCL_nn2_round_1_clusters.txt -tabr veg_clr_nn2_graph_round_1.dict 
 
 # Now for the sexual case
-mcx query -imx sex_clr_nn3_graph_round_1.mci -vary-threshold 7/20/130
+mcx query -imx sex_clr_nn2_graph_round_1.mci -vary-threshold 7/20/130
 
 # Use a cutoff of 15.9
-mcx alter -imx sex_clr_nn3_graph_round_1.mci -tf 'gq(15.9), add(-15.9)' -o sex_clr_nn3_graph_round_1_15.9.mci
+mcx alter -imx sex_clr_nn2_graph_round_1.mci -tf 'gq(15.9), add(-15.9)' -o sex_clr_nn2_graph_round_1_15.9.mci
 
-mcl sex_clr_nn3_graph_round_1_15.9.mci -I 1.2 -scheme 7
+mcl sex_clr_nn2_graph_round_1_15.9.mci -I 1.2 -scheme 7
 
-mcl sex_clr_nn3_graph_round_1_15.9.mci -I 1.4 -scheme 7
+mcl sex_clr_nn2_graph_round_1_15.9.mci -I 1.4 -scheme 7
 
-mcl sex_clr_nn3_graph_round_1_15.9.mci -I 1.6 -scheme 7
+mcl sex_clr_nn2_graph_round_1_15.9.mci -I 1.6 -scheme 7
 
-mcl sex_clr_nn3_graph_round_1_15.9.mci -I 1.8 -scheme 7
+mcl sex_clr_nn2_graph_round_1_15.9.mci -I 1.8 -scheme 7
 
-mcl sex_clr_nn3_graph_round_1_15.9.mci -I 2 -scheme 7
+mcl sex_clr_nn2_graph_round_1_15.9.mci -I 2 -scheme 7
 
-mcl sex_clr_nn3_graph_round_1_15.9.mci -I 2.2 -scheme 7
+mcl sex_clr_nn2_graph_round_1_15.9.mci -I 2.2 -scheme 7
 
-mcl sex_clr_nn3_graph_round_1_15.9.mci -I 2.4 -scheme 7
+mcl sex_clr_nn2_graph_round_1_15.9.mci -I 2.4 -scheme 7
 
-mcl sex_clr_nn3_graph_round_1_15.9.mci -I 2.6 -scheme 7
+mcl sex_clr_nn2_graph_round_1_15.9.mci -I 2.6 -scheme 7
 
-mcl sex_clr_nn3_graph_round_1_15.9.mci -I 2.8 -scheme 7
+mcl sex_clr_nn2_graph_round_1_15.9.mci -I 2.8 -scheme 7
 
-mcl sex_clr_nn3_graph_round_1_15.9.mci -I 3 -scheme 7
+mcl sex_clr_nn2_graph_round_1_15.9.mci -I 3 -scheme 7
 
-clm info sex_clr_nn3_graph_round_1_15.9.mci out.sex_clr_nn3_graph_round_1_15.9.mci.I{12,14,16,18,20,22,24,26,28,30}
-clm dist --chain out.sex_clr_nn3_graph_round_1_15.9.mci.I{12,14,16,18,20,22,24,26,28,30}
+clm info sex_clr_nn2_graph_round_1_15.9.mci out.sex_clr_nn2_graph_round_1_15.9.mci.I{12,14,16,18,20,22,24,26,28,30}
+clm dist --chain out.sex_clr_nn2_graph_round_1_15.9.mci.I{12,14,16,18,20,22,24,26,28,30}
 
 # Sticking with -I 1.8 again efficiency=0.55142 massfrac=0.38185 areafrac=0.00167
-mcxdump -icl out.sex_clr_nn3_graph_round_1_15.9.mci.I18 -o sex_labeled_MCL_round_1_clusters.txt -tabr sex_clr_nn3_graph_round_1.dict 
+mcxdump -icl out.sex_clr_nn2_graph_round_1_15.9.mci.I18 -o sex_labeled_MCL_nn2_round_1_clusters.txt -tabr sex_clr_nn2_graph_round_1.dict 
--- TGNE/eggnog/eggnog_setup.sh
+++ TGNE/eggnog/eggnog_setup.sh
@@ -4,15 +4,16 @@
 # To download the necessary data:
 # Don't download Diamond DB
 # Keep default as the DB (for some reason specifying Eukaryota didn't work)
-download_eggnog_data.py -H -d 2759 # fails after downloading hmmer data because of differences between GNU and Mac OS X mv and zcat commands
+download_eggnog_data.py -H -d 2759 -P -f -y # fails after downloading hmmer data because of differences between GNU and Mac OS X mv and zcat commands
 
 # Install coreutils using brew. Rerun failed commands with gmv and gzcat
-cd /Users/eukarya/eggnog-mapper-data/hmmer/2759; echo Downloading HMMs... && wget -N -nH --user-agent=Mozilla/5.0 --relative -r --no-parent --reject "index.html*" --cut-dirs=4 -e robots=off http://eggnog5.embl.de/download/eggnog_5.0/per_tax_level/2759//2759_hmms.tar.gz && echo Decompressing HMMs... && tar zxf 2759_hmms.tar.gz && echo 2759/* | xargs gmv -t ./ && rm -r 2759 && rm 2759_hmms.tar.gz; numf=$(find ./ | grep -c ".hmm$"); curr=0; cat /dev/null > 2759.hmm_tmp; for file in $(find ./ | grep ".hmm$"); do curr=$((curr+1)); echo "merging HMMs... ${file} (${curr}/${numf})"; cat "${file}" | sed -e "s/.faa.final_tree.fa//" -e "s/.faa.final_tree//" >> 2759.hmm_tmp; rm "${file}"; done; mv 2759.hmm_tmp 2759.hmm; (if [ -f 2759.hmm.h3i ]; then rm 2759.hmm.h3*; fi) && echo "hmmpress-ing HMMs... " && /anaconda3/envs/eggnog/bin/hmmpress 2759.hmm && echo "generating idmap file... " && cat 2759.hmm | grep "^NAME" | sed -e "s/^NAME *//" | awk '{print NR"    "$0}' > 2759.hmm.idmap && echo "removing single OG hmm files... " && echo ./*hmm | xargs rm;
+# NOTE: my hmmpress is in my miniconda3 environment; yours might be somewhere else!
+cd /Users/eukarya/eggnog-mapper-data/hmmer/2759; echo Downloading HMMs... && wget -N -nH --user-agent=Mozilla/5.0 --relative -r --no-parent --reject "index.html*" --cut-dirs=4 -e robots=off http://eggnog5.embl.de/download/eggnog_5.0/per_tax_level/2759//2759_hmms.tar.gz && echo Decompressing HMMs... && tar zxf 2759_hmms.tar.gz && echo 2759/* | xargs gmv -t ./ && rm -r 2759 && rm 2759_hmms.tar.gz; numf=$(find ./ | grep -c ".hmm$"); curr=0; cat /dev/null > 2759.hmm_tmp; for file in $(find ./ | grep ".hmm$"); do curr=$((curr+1)); echo "merging HMMs... ${file} (${curr}/${numf})"; cat "${file}" | sed -e "s/.faa.final_tree.fa//" -e "s/.faa.final_tree//" >> 2759.hmm_tmp; rm "${file}"; done; mv 2759.hmm_tmp 2759.hmm; (if [ -f 2759.hmm.h3i ]; then rm 2759.hmm.h3*; fi) && echo "hmmpress-ing HMMs... " && /miniconda3/envs/eggnog/bin/hmmpress 2759.hmm && echo "generating idmap file... " && cat 2759.hmm | grep "^NAME" | sed -e "s/^NAME *//" | awk '{print NR"    "$0}' > 2759.hmm.idmap && echo "removing single OG hmm files... " && echo ./*hmm | xargs rm;
 
 cd /Users/eukarya/eggnog-mapper-data/hmmer/2759; echo Downloading FASTAs... && wget -N -nH --user-agent=Mozilla/5.0 --relative -r --no-parent --reject "index.html*" --cut-dirs=4 -e robots=off http://eggnog5.embl.de/download/eggnog_5.0/per_tax_level/2759//2759_raw_algs.tar && echo Decompressing FASTAs... && tar xf 2759_raw_algs.tar && echo 2759/* | xargs gmv -t ./ && rm -r 2759 && rm 2759_raw_algs.tar; numf=$(find ./ | grep -c ".faa.gz$"); curr=0; for file in $(find ./ | grep ".faa.gz$"); do curr=$((curr+1)); echo "processing FASTAs...  ${file} (${curr}/${numf})"; outf=$(echo "$file" | sed "s/\.raw_alg\.faa\.gz/\.fa/"); gzcat "$file" | awk '/^>/{print; next}{gsub("-", ""); print}' > "$outf" && rm "$file"; done
 
 # Get the pfams
-download_eggnog_data.py -P -f
+# download_eggnog_data.py -P -f
 
 # Run eggnog-mapper
 emapper.py  -m hmmer -d 2759 -i ../../raw_data/Tthermophila_MAC_protein_2021.fasta -o Ttherm2021MAC_eggnog_hmmer_default_evalue --cpu 0 --tax_scope Eukaryota --target_taxa 2759 --report_orthologs --report_no_hits --go_evidence non-electronic --pfam_realign realign --dbtype hmmdb --dbmem
