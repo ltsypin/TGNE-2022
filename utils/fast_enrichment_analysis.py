1444041752f134c79bc1b59d34f0e603564ccf4e
mbertagna <78129017+mbertagna@users.noreply.github.com>
2024-04-04 11:12:08 -0500

    Added more metrics to TGNE/clustering_optimization/fast_scan_modular_metrics.py

--- utils/fast_enrichment_analysis.py
+++ utils/fast_enrichment_analysis.py
@@ -8,47 +8,16 @@ import sys
 import pickle
 from multiprocessing import Pool
 
+import annotation_info
+
 # ENRICHMENT_ESSENTIAL_FINAL
 
 main_dir = '../TGNE/'
 
 complete_annotation = pd.read_csv(os.path.join(main_dir, 'eggnog/complete_eggnog_annotation.csv'))
-go_df = pd.read_csv(os.path.join(main_dir, '../active_fastas/go_annotations.csv'))
-kegg_df = pd.read_csv(os.path.join(main_dir, '../active_fastas/kegg_annotations.csv'))
-ec_df = pd.read_csv(os.path.join(main_dir, '../active_fastas/ec_annotations.csv'))
 
 background_annotation = complete_annotation
 term_columns=['COG_category', 'GOs', 'KEGG_ko', 'EC', 'PFAMs']
-
-# As of 2020 https://www.ncbi.nlm.nih.gov/research/cog/
-COG_dict = {
-    "A" : "RNA processing and modification",
-    "B" : "Chromatin structure and dynamics",
-    "C" : "Energy production and conversion",
-    "D" : "Cell cycle control, cell division, chromosome partitioning",
-    "E" : "Amino acid transport and metabolism",
-    "F" : "Nucleotide transport and metabolism",
-    "G" : "Carbohydrate transport and metabolism",
-    "H" : "Coenzyme transport and metabolism",
-    "I" : "Lipid transport and metabolism",
-    "J" : "Translation, ribosomal structure and biogenesis",
-    "K" : "Transcription",
-    "L" : "Replication, recombination, and repair",
-    "M" : "Cell wall/membrane/envelope biogenesis",
-    "N" : "Cell motility",
-    "O" : "Posttranslational modification, protein turnover, chaperones",
-    "P" : "Inorganic ion transport and metabolism",
-    "Q" : "Secondary metabolites biosynthesis, transport and catabolism",
-    "T" : "Signal transduction mechanisms",
-    "U" : "Intracellular trafficking, secretion, and vesicular transport",
-    "V" : "Defense mechanisms",
-    "W" : "Extracellular structures",
-    "X" : "Mobilome: prophages, transposons",
-    "Y" : "Nuclear structure",
-    "Z" : "Cytoskeleton",
-    "R" : "General function prediction only",
-    "S" : "Function unknown",
-}
 
 def term_count_dict_from_annotation_df(annot_df, term_column):
     
@@ -133,22 +102,6 @@ def enrichment_analysis(module, label_df, background_annotation, term_column):
 #             terms.append('')
             
     return ps, bs, folds, terms
-            
-def get_GO_info(go_term):
-    
-    name = go_df['GOs_description'].loc[go_df['GOs'] == go_term].values[0]
-    
-    definition = go_df['GOs_definition'].loc[go_df['GOs'] == go_term].values[0]
-    
-    obsolete = go_df['GOs_obsolete'].loc[go_df['GOs'] == go_term].values[0]
-    
-    return name, definition, obsolete
-
-def get_KEGG_info(term):
-    return kegg_df['KEGG_ko_description'].loc[kegg_df['KEGG_ko'] == term].values[0]
-
-def get_EC_info(term):
-    return ec_df['EC_description'].loc[ec_df['EC'] == term].values[0]
         
 # ENRICHMENT_ESSENTIAL_FINAL END
 def init_pool(data):
@@ -165,7 +118,7 @@ def process_module(m):
 
         if tc == 'GOs':
             for t in terms:
-                name, definition, obsolete = get_GO_info(t)
+                name, definition, obsolete = annotation_info.get_GO_info(t)
                 if obsolete:
                     info.append(f'{name.capitalize()}: {definition} (obsolete)')
                 else:
@@ -173,15 +126,15 @@ def process_module(m):
                     
         elif tc == 'COG_category':
             for t in terms:
-                info.append(COG_dict[t])
+                info.append(annotation_info.COG_dict[t])
                             
         elif tc == 'KEGG_ko':
             for t in terms:
-                info.append(get_KEGG_info(t))
+                info.append(annotation_info.get_KEGG_info(t))
                 
         elif tc == 'EC':
             for t in terms:
-                info.append(get_EC_info(t))
+                info.append(annotation_info.get_EC_info(t))
 
         elif tc == 'PFAMs':
             for t in terms:
