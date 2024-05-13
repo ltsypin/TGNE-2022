2b59d47bc7702ca1101e9e1acf43ff720c7d1e86
mbertagna <78129017+mbertagna@users.noreply.github.com>
2024-05-13 15:41:29 -0500

    Update mds.py

--- TGNE/embedding/mds.py
+++ TGNE/embedding/mds.py
@@ -40,8 +40,6 @@ def nmds_embedding(full_filtered_norm_df: pd.DataFrame, file_name: str):
 full_filtered_df = pd.read_csv(os.path.join(file_dir, '../../active_fastas/rna_seq.csv'))
 full_filtered_norm_df = rna_seq_utils.normalize_expression_per_gene(full_filtered_df)
 
-full_filtered_norm_df = full_filtered_norm_df.sample(500)
-
 mds_embedding(full_filtered_norm_df, 'rna_seq_mds.csv')
 
 nmds_embedding(full_filtered_norm_df, 'rna_seq_nmds.csv')
@@ -50,8 +48,6 @@ nmds_embedding(full_filtered_norm_df, 'rna_seq_nmds.csv')
 full_filtered_df = pd.read_csv(os.path.join(file_dir, '../microarray_probe_alignment_and_filtering/allgood_filt_agg_tidy_2021aligned_qc_rma_expression_full.csv'))
 full_filtered_df = full_filtered_df.rename(columns={'Unnamed: 0': 'TTHERM_ID'})
 full_filtered_norm_df = microarray_utils.normalize_expression_per_gene(full_filtered_df, z=True)
-
-full_filtered_norm_df = full_filtered_norm_df.sample(500)
 
 mds_embedding(full_filtered_norm_df, 'microarray_mds.csv')
 
