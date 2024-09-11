242198e4a0bee38fdc19e7b88aa33cdf467ebb4f
mbertagna <78129017+mbertagna@users.noreply.github.com>
2024-09-11 12:55:10 -0500

    Update build_tgne_env_silicon.sh

--- build_tgne_env_silicon.sh
+++ build_tgne_env_silicon.sh
@@ -13,6 +13,6 @@ conda config --set channel_priority strict
 
 conda config --env --set subdir osx-64
 
-conda install pandas matplotlib numpy scikit-learn umap-learn python-igraph leidenalg networkx scipy bokeh openpyxl distinctipy jupyter gdown tqdm biopython=1.76 psutil=5.7.0 xlsxwriter=1.4.3 eggnog-mapper r-base r-ggplot2 r-bigmemory r-snow r-rmarkdown r-tidyr r-rcolorbrewer r-actuar r-fitdistrplus r-hmisc r-wgcna r-biocmanager bioconductor-geneplotter bioconductor-oligo bioconductor-pdinfobuilder bioconductor-masigpro bioconductor-limma r-dplyr r-ggrepel r-kernsmooth -y
+conda install pandas matplotlib matplotlib-venn numpy scikit-learn umap-learn python-igraph leidenalg networkx scipy bokeh openpyxl distinctipy jupyter gdown tqdm biopython=1.76 psutil=5.7.0 xlsxwriter=1.4.3 eggnog-mapper r-base r-ggplot2 r-bigmemory r-snow r-rmarkdown r-tidyr r-rcolorbrewer r-actuar r-fitdistrplus r-hmisc r-wgcna r-biocmanager bioconductor-geneplotter bioconductor-oligo bioconductor-pdinfobuilder bioconductor-masigpro bioconductor-limma r-dplyr r-ggrepel r-kernsmooth -y
 
 Rscript -e 'install.packages("admisc", dependencies=TRUE, repos="https://cran.rstudio.com/")'
